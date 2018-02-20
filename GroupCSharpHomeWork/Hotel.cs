f58e84de34a1d5c936fe570a2b83471c9f03f906
JuliePretzlaff <juliapretzlaff@gmail.com>
2018-02-19 20:56:04 -0500

    added hotel class

---
+++ GroupCSharpHomeWork/Hotel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupCSharpHomeWork
+{
+    public class Hotel
+    {
+       
+        public string HotelChoice(string beach, double shoes)
+        {
+            if (beach == "You're going to Tahiti!")
+            {
+                switch (shoes)
+                {
+                    case 6.5:
+                        return "You're staying at Days Inn.";
+                        break;
+
+                    case 10.5:
+                        return "You're staying at the Ritz.";
+                        break;
+
+                    default:
+                        return "You have an abnormal shoes size.";
+                        break;
+                }
+            }
+            else if (beach == "You're going to Zanzibar!")
+            {
+                switch (shoes)
+                {
+                    case 6.5:
+                        return "You're staying at the Beach Shell Inn.";
+                        break;
+
+                    case 10.5:
+                        return "You're staying at the Stonetown Hotel.";
+                        break;
+
+                    default:
+                        return "You have an abnormal shoes size.";
+                        break;
+                }
+            }
+           else if (beach == "You're going to California!")
+            {
+                switch (shoes)
+                {
+                    case 6.5:
+                        return "You're staying at the Pink Motel.";
+                        break;
+
+                    case 10.5:
+                        return "You're staying at the Hotel California.";
+                        break;
+
+                    default:
+                        return "You have an abnormal shoes size.";
+                        break;
+                }
+            }
+            else
+            {
+                return "You have an abnormal shoe size.";
+            }
+
+
+        }
+    }
+}
--- GroupCSharpHomeWork/Program.cs
+++ GroupCSharpHomeWork/Program.cs
@@ -11,9 +11,15 @@ namespace GroupCSharpHomeWork
         static void Main(string[] args)
         {
             BeachResult beachOne = new BeachResult();
+            Hotel hotelOne = new Hotel();
 
             Console.WriteLine("Please enter your eye color as one of the following:  blue, green, brown.");
-            Console.WriteLine(beachOne.NumberToBeach(beachOne.ColorToNumber(Console.ReadLine().ToLower())));
+            string beach = (beachOne.NumberToBeach(beachOne.ColorToNumber(Console.ReadLine().ToLower())));
+            Console.WriteLine(beach);
+
+            Console.WriteLine("\nWhat is your shoe size?");
+            double size = double.Parse(Console.ReadLine());
+            Console.WriteLine(hotelOne.HotelChoice(beach, size));
         }
     }
 }
